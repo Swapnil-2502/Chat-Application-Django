207ff6c7f3b094c607d72f18ad1658ed700ce160
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2023-10-24 12:22:13 +0530

    added register page

--- chat/signals.py
+++ chat/signals.py
@@ -2,9 +2,19 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Userprofilemodel
 import json
+from django.contrib.auth.models import User
 
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Userprofilemodel.objects.create(user=instance)
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.Userprofilemodel.save()
 
 @receiver(post_save, sender=Userprofilemodel)
 def send_onlineStatus(sender, instance, created, **kwargs):
--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def login_user(request):
     
@@ -28,3 +29,22 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You Were Logged Out!"))
     return redirect('login')
+
+def register_user(request):
+    
+    form = UserCreationForm(request.POST)
+    
+    if form.is_valid():
+        form.save()
+        
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+        user = authenticate(username=username,password=password)
+        login(request,user)
+        messages.success(request,'Your registration was successfull')
+        return redirect('home')
+    
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'authenticate/register.html',{'form':form})
